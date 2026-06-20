e89ea6478d4f86dc2908ef0b86f1e03356264867
Thineth Shehara <sheharanayanananda@gmail.com>
2026-06-20 14:19:00 +0530

    UI changes

---
+++ Slate/Features/Chat/ChatView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatView.swift
+//  Slate
+//
+//  Created by Thineth Shehara on 2026-06-20.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatView()
+}
